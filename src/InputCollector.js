2fc3b10e73b2e38af1312673109fea2894530aac
candyzje <reap2121@gmail.com>
2019-03-22 20:59:15 +0100

    Added possibility to fill table with possible solutions and acquire them

--- src/InputCollector.js
+++ src/InputCollector.js
@@ -1,5 +1,34 @@
+var possibleSolutionsMatrix = [];
+
 var getElementValue = (id) => {
     var value = document.getElementById(id).innerHTML;
     console.log(value);
 }
 
+var getElementPossibleValues = (id) => {
+    var element;
+
+    possibleSolutionsMatrix.forEach(matrixElement => {
+        if(matrixElement.id == id)
+        {
+            element = matrixElement;
+        }
+    });
+    console.log("id: " + element.id)
+    console.log("values: " + element.possibleValues);
+}
+
+var fillPossibleSolutionsMatrix = () => {
+    for(let i = 0; i < 9; i++)
+    {
+        for(let j = 0; j < 9; j++)
+        {
+            possibleSolutionsElement = {
+                id: `${i}-${j}`,
+                possibleValues: "123456789"
+            };
+
+            possibleSolutionsMatrix.push(possibleSolutionsElement);
+        }
+    }
+}
